ab95ed3ea300cb0989ec0bebdba3a57f3a335a46
pointergy <pointergy@gmail.com>
2023-02-26 01:30:23 +0100

    Zadanie dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿var name = "Ewa";
+var sex = 'F';
+var age = 14;
+
+if (name == "Ewa" && sex == 'F' && age == 33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (sex == 'F' && age < 30)
+{
+    Console.WriteLine("Kobieta, poniżej 30 lat");
+}
+else if (sex == 'M' && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
